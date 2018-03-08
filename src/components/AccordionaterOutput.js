1da03aba214a26112d88628efc16bce446183926
Michael <mxsnydeasdr@baltimorecountymd.gov>
2018-03-08 15:33:51 -0500

    Set up copy to clipboard button

--- src/components/AccordionaterOutput.js
+++ src/components/AccordionaterOutput.js
@@ -1,14 +1,43 @@
 import React, { Component } from 'react';
 import pretty from 'pretty';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 class AccordionaterOutput extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			isCopied: false
+		};
+
+		this.handleCopyClick = this.handleCopyClick.bind(this);
+		this.renderCopiedNote = this.renderCopiedNote.bind(this);
+	}
+
+	handleCopyClick() {
+		this.setState({ isCopied: true });
+	}
+
+	renderCopiedNote() {
+		if (this.state.isCopied) {
+			setTimeout(() => this.setState({ isCopied: false }), 2000);
+
+			return (<strong>Copied!</strong>);
+		}
+	}
+
 	render() {
 		return (
 			<div className="row">
 				<div className="col-md-12">
 					<div className="well">
 						<div className="form-group">
-							<h4>Generated code</h4>
+							<h4>Generated code
+								<CopyToClipboard onCopy={this.handleCopyClick} text={pretty(this.props.value)}>
+									<button className="btn btn-default generated-code-button">Copy to clipboard</button>
+								</CopyToClipboard>
+								{this.renderCopiedNote()}
+							</h4>
 							<pre>
 								<code>{pretty(this.props.value)}</code>
 							</pre>

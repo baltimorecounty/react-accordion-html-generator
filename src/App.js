4e34a49aa604a2865bc83901f126da5ab8e8589d
martypowell <jpowel25@gmail.com>
2019-03-01 09:45:52 -0500

    reformat

--- src/App.js
+++ src/App.js
@@ -1,84 +1,88 @@
-import React, { Component } from "react";
-import { Accordionater, AccordionaterOutput } from "./components";
-import AppCss from "./App.css"; // eslint-disable-line
+import React, { Component } from 'react';
+import { Accordionater, AccordionaterOutput } from './components';
+import AppCss from './App.css'; // eslint-disable-line
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      accordionatorHtmlOutput: [],
-      code: "",
-      accordionTitleForIds: "",
-      headingType: "h4"
-    };
+        this.state = {
+            accordionatorHtmlOutput: [],
+            code: '',
+            accordionTitleForIds: '',
+            headingType: 'h4'
+        };
 
-    this.handleWizzyChange = this.handleWizzyChange.bind(this);
-    this.handleGenerateCodeClick = this.handleGenerateCodeClick.bind(this);
-    this.handleAccordionTitleChange = this.handleAccordionTitleChange.bind(
-      this
-    );
-    this.handleHeadingTypeChange = this.handleHeadingTypeChange.bind(this);
-    this.generateHtmlFromTemplate = this.generateHtmlFromTemplate.bind(this);
-  }
+        this.handleWizzyChange = this.handleWizzyChange.bind(this);
+        this.handleGenerateCodeClick = this.handleGenerateCodeClick.bind(this);
+        this.handleAccordionTitleChange = this.handleAccordionTitleChange.bind(
+            this
+        );
+        this.handleHeadingTypeChange = this.handleHeadingTypeChange.bind(this);
+        this.generateHtmlFromTemplate = this.generateHtmlFromTemplate.bind(
+            this
+        );
+    }
 
-  handleAccordionTitleChange(accordionTitle) {
-    this.setState({
-      accordionTitleForIds: this.cleanStringForId(accordionTitle)
-    });
-  }
+    handleAccordionTitleChange(accordionTitle) {
+        this.setState({
+            accordionTitleForIds: this.cleanStringForId(accordionTitle)
+        });
+    }
 
-  handleGenerateCodeClick() {
-    const output = this.state.accordionatorHtmlOutput.map(
-      this.generateHtmlFromTemplate
-    );
+    handleGenerateCodeClick() {
+        const output = this.state.accordionatorHtmlOutput.map(
+            this.generateHtmlFromTemplate
+        );
 
-    const code = `<!-- BEGIN ${this.state.accordionTitleForIds} ACCORDION -->
+        const code = `<!-- BEGIN ${
+            this.state.accordionTitleForIds
+        } ACCORDION -->
 		<div aria-multiselectable="true" class="content-accordion" id="${
-      this.state.accordionTitleForIds
-    }" role="tablist">${output.join("")}</div>
+            this.state.accordionTitleForIds
+        }" role="tablist">${output.join('')}</div>
 		<!-- END ${this.state.accordionTitleForIds} ACCORDION -->`;
 
-    this.setState({ code });
-  }
-
-  handleHeadingTypeChange(headingType) {
-    this.setState({ headingType });
-  }
-
-  handleWizzyChange(title, body, index) {
-    const outputArrayCopy = this.state.accordionatorHtmlOutput.slice();
-
-    if (!outputArrayCopy[index]) {
-      while (index < outputArrayCopy.length) {
-        outputArrayCopy.push("");
-      }
+        this.setState({ code });
     }
 
-    outputArrayCopy[index] = {
-      title: title || "",
-      body: body || ""
-    };
-
-    this.setState({ accordionatorHtmlOutput: outputArrayCopy });
-  }
-
-  cleanStringForId(value) {
-    const justNumbersAndLettersRegex = /\w+/g;
-    const matches = [];
-    let match;
-
-    while ((match = justNumbersAndLettersRegex.exec(value)) !== null) {
-      matches.push(match[0]);
+    handleHeadingTypeChange(headingType) {
+        this.setState({ headingType });
     }
 
-    return matches.join("");
-  }
+    handleWizzyChange(title, body, index) {
+        const outputArrayCopy = this.state.accordionatorHtmlOutput.slice();
 
-  generateHtmlFromTemplate(outputObject, index) {
-    const panelTitle = this.cleanStringForId(outputObject.title);
+        if (!outputArrayCopy[index]) {
+            while (index < outputArrayCopy.length) {
+                outputArrayCopy.push('');
+            }
+        }
 
-    return `<div class="panel">
+        outputArrayCopy[index] = {
+            title: title || '',
+            body: body || ''
+        };
+
+        this.setState({ accordionatorHtmlOutput: outputArrayCopy });
+    }
+
+    cleanStringForId(value) {
+        const justNumbersAndLettersRegex = /\w+/g;
+        const matches = [];
+        let match;
+
+        while ((match = justNumbersAndLettersRegex.exec(value)) !== null) {
+            matches.push(match[0]);
+        }
+
+        return matches.join('');
+    }
+
+    generateHtmlFromTemplate(outputObject, index) {
+        const panelTitle = this.cleanStringForId(outputObject.title);
+
+        return `<div class="panel">
 			<${this.state.headingType} id="${panelTitle}-heading-${index}">
 				<a aria-controls="${panelTitle}-panel-${index}"
 					aria-expanded="false"
@@ -96,32 +100,32 @@ class App extends Component {
 			</div>
 		</div>
 		`;
-  }
+    }
 
-  render() {
-    return (
-      <div className="accordionater container-fluid">
-        <div className="row">
-          <div className="col-md-12">
-            <h1>Accordionater</h1>
-            <p>
-              Welcome to Accordionater, the bootstrap accordion generator for
-              the Baltimore County sites.
-            </p>
-          </div>
-        </div>
+    render() {
+        return (
+            <div className="accordionater container-fluid">
+                <div className="row">
+                    <div className="col-md-12">
+                        <h1>Accordionater</h1>
+                        <p>
+                            Welcome to Accordionater, the bootstrap accordion
+                            generator for the Baltimore County sites.
+                        </p>
+                    </div>
+                </div>
 
-        <Accordionater
-          onWizzyChange={this.handleWizzyChange}
-          onGenerateCodeClick={this.handleGenerateCodeClick}
-          onAccordionTitleChange={this.handleAccordionTitleChange}
-          onHeadingTypeChange={this.handleHeadingTypeChange}
-        />
-        <br />
-        <AccordionaterOutput value={this.state.code} />
-      </div>
-    );
-  }
+                <Accordionater
+                    onWizzyChange={this.handleWizzyChange}
+                    onGenerateCodeClick={this.handleGenerateCodeClick}
+                    onAccordionTitleChange={this.handleAccordionTitleChange}
+                    onHeadingTypeChange={this.handleHeadingTypeChange}
+                />
+                <br />
+                <AccordionaterOutput value={this.state.code} />
+            </div>
+        );
+    }
 }
 
 export default App;
